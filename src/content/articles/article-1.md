5e34ed85220ccbec09b470159ca58a69733067fe
Wiibleyde <nathan@bonnell.fr>
2024-04-24 11:32:28 +0200

    Fix article load and and a template article
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ src/content/articles/article-1.md
@@ -0,0 +1,9 @@
+---
+inProgress: false
+title: Introdution
+description: '<p>Voici le premier article de mon blog dans lequel vous allez en apprendre plus sur mon stage au sein de .</p>'
+img_alt: company-logo
+img_src: https://nathan.bonnell.fr/cv/src/pp.jpg
+link: /blog/1 # DON'T FORGET TO CHANGE THIS LINK
+tags: ['FirstDay', 'Stage', 'Introduction']
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,18 @@ const projectsCollection = defineCollection({
 	}),
 });
 
+const blogCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		date: z.string(),
+		tags: z.array(z.string()),
+		link: z.string(),
+		img_alt: z.string().optional(),
+		img_src: z.string().optional(),
+	}),
+});
+
 export const collections = {
 	projects: projectsCollection,
+	blog: blogCollection,
 };
